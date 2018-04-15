0da2187c4c9a8b7f0a3841b21a48a8caddf333b3
Dawid Stefaniak <contact@dawidstefaniak.com>
2018-04-15 16:25:19 +0100

    Update MessagesController.cs
    Removed commented part

--- MessangesAPI/Controllers/MessagesController.cs
+++ MessangesAPI/Controllers/MessagesController.cs
@@ -45,49 +45,6 @@ namespace MessangesAPI.Controllers
             return Ok();
         }
 
-        //Out of date (sender and receiver ID will be in JSON)
-        /*  
-        [HttpPost("add/{senderId}/{receiverId}")]
-        public IActionResult AddMessage(int senderId, int receiverId, [FromBody] MessageForCreationDto message)
-        {
-            if (message == null)
-            {
-                return BadRequest();
-            }
-
-            if(senderId == receiverId)
-            {
-                ModelState.AddModelError("WrongId", "You cannot send message to yourself!");
-            }
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            if (!_messangerRepository.UserExists(senderId) || !_messangerRepository.UserExists(receiverId))
-            {
-                return NotFound();
-            }
-
-            //Message which is transformed from ForCreation object to Message object
-            var finalMessage = AutoMapper.Mapper.Map<Entities.Message>(message);
-
-            //Adding message to DB
-            _messangerRepository.AddMessage(receiverId, senderId, finalMessage);
-
-            //Saving changes
-            if (!_messangerRepository.Save())
-            {
-                return StatusCode(500, "Problem while handling your request.");
-            }
-            //For now is useless
-            var createdMessageToReturn = AutoMapper.Mapper.Map<Models.MessageDto>(finalMessage);
-
-            return Ok();
-        }
-        */
-
         [HttpGet("getmessages/{userId}")]
         public IActionResult GetMessages(int userId)
         {
