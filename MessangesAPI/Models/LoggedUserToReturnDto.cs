1e7d8b6566a78ce035a1085e68557ac61d3bf229
Dawid Stefaniak <contact@dawidstefaniak.com>
2018-04-15 16:29:01 +0100

    Update LoggedUserToReturnDto.cs
    Repair bug with wrong usertype json return

--- MessangesAPI/Models/LoggedUserToReturnDto.cs
+++ MessangesAPI/Models/LoggedUserToReturnDto.cs
@@ -4,8 +4,6 @@ namespace MessangesAPI.Models
     public class LoggedUserToReturnDto
     {
         public int UserId { get; set; }
-        public char UserType {get;set;}
-        //public static char UserType { get; set;}
-
+        public string UserType {get;set;}
     }
 }
